85f9fd5358a79fc238351c464021550f7765c745
MR14Titan14 <sansen184@gmail.com>
2024-09-19 14:59:01 +0300

    2.2 Добавлена фильтрация с помощью inRange

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 
+#Задание 1,2
 cap=cv.VideoCapture(0)
 frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -8,7 +9,9 @@ while True:
     if not(ret):
         break
     frame=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-    cv.imshow('mp4',frame)
+    frame_trash=cv.inRange(frame,(0,100,100),(30,255,255))
+    print(frame[int(frame_width/2)][int(frame_height/2)])
+    cv.imshow('mp4',frame_trash)
     if cv.waitKey(1) & 0XFF == 27:
         break
 cv.destroyAllWindows()
