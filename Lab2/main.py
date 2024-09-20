1537a7780972753d890352f045d86f0bdfc331ff
MR14Titan14 <sansen184@gmail.com>
2024-09-20 06:14:02 +0300

    2. Правки

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 import numpy as np
 
@@ -9,9 +10,10 @@ import numpy as np
 #     ret, frame = cap.read()
 #     if not(ret):
 #         break
-#     frame=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-#     frame_trash=cv.inRange(frame,(0,100,100),(30,255,255))
-#     print(frame[int(frame_width/2)][int(frame_height/2)])
+#     frame_hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+#     mask=cv.inRange(frame_hsv,(0,155,155),(30,255,255))
+#     frame_trash=cv2.bitwise_and(frame,frame,mask=mask)
+#     #print(frame[int(frame_width/2)][int(frame_height/2)])
 #     cv.imshow('filtered',frame_trash)
 #     if cv.waitKey(1) & 0XFF == 27:
 #         break
@@ -25,7 +27,7 @@ import numpy as np
 #     if not(ret):
 #         break
 #     frame=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
-#     frame_trash=cv.inRange(frame,(0,100,100),(30,255,255))
+#     frame_trash=cv.inRange(frame,(0,155,155),(30,255,255))
 #     opened=cv.morphologyEx(frame_trash,cv.MORPH_OPEN,kernel)
 #     closed=cv.morphologyEx(frame_trash,cv.MORPH_CLOSE,kernel)
 #     cv.imshow('Open',opened)
@@ -48,14 +50,13 @@ while True:
     if(openmoments["m00"]!=0):
         print(f"Площадь: {openmoments['m00']}")
         print(f"Моменты 1 порядка: {openmoments['m01']}, {openmoments['m10']}")
-        x=int(openmoments['m10']/openmoments['m00'])
-        y=int(openmoments['m01']/openmoments['m00'])
-        cv.circle(frame,(x,y),10,(0,0,0,),2)
+        xc=int(openmoments['m10']/openmoments['m00'])
+        yc=int(openmoments['m01']/openmoments['m00'])
         (contours, _) = cv.findContours(opened.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
         for countour in contours:
             (x, y, w, h) = cv.boundingRect(countour)
             cv.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 4)
-    cv.imshow('Detecting',frame)
+    cv.imshow('Detecting red',frame)
     if cv.waitKey(1) & 0XFF == 27:
         break
 cv.destroyAllWindows()
