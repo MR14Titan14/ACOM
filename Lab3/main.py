2aa4303e2d33727cdcb019df0864bf9f8528f79a
MR14Titan14 <sansen184@gmail.com>
2024-09-20 04:52:35 +0300

    3.3,4 Добавлена реализация фильтра Гаусса

--- Lab3/main.py
+++ Lab3/main.py
@@ -19,5 +19,28 @@ def gausianBlur(img,kernelSize,devation):
     kernel/=sum
     print(kernel)
 
-    
-gausianBlur(5,10)
+    blured=img.copy()
+    Start = kernelSize // 2
+
+    for i in range(Start, blured.shape[0] - Start):
+        for j in range(Start, blured.shape[1] - Start):
+            val=0
+            for k in range(-Start, Start + 1):
+                for l in range(-Start, Start + 1):
+                    val += img[i + k, j + l] * kernel[k + Start, l + Start]
+            blured[i, j] = val
+    return blured
+
+img=cv.imread("jak.jpeg",cv.IMREAD_GRAYSCALE)
+blured1=gausianBlur(img,5,100)
+blured2=gausianBlur(img,7,100)
+blured3=gausianBlur(img,5,10)
+blured4=gausianBlur(img,7,10)
+
+cv.imshow("blured 5 100",blured1)
+cv.imshow("blured 7 100",blured2)
+cv.imshow("blured 5 10",blured3)
+cv.imshow("blured 7 10",blured4)
+
+cv.waitKey(0) # Wait for a keystroke in the window
+cv.destroyAllWindows()
