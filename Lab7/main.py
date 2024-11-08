1c9b393e0e9a603992fd41ad5480f312d3f1828b
MR14Titan14 <sansen184@gmail.com>
2024-11-08 03:25:27 +0300

    7.3 Добавлен rec_type augmented recognition, произведены тесты

--- Lab7/main.py
+++ Lab7/main.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import os
 import pytesseract as tes
-import easyocr
 from jiwer import wer
+from collections import Counter
+import easyocr
 
 tes.pytesseract.tesseract_cmd=(r"C:\Program Files\Tesseract-OCR\tesseract.exe")
 
@@ -63,8 +64,44 @@ def test_recognition(rec_type,val_type,ds_dir):
             img=cv2.medianBlur(img,3)
             text=tes.image_to_string(img,lang="rus+eng")
             res[name[:-4]]=str(text).replace("\n","")
-        print(res)
-        print(labels)
+    if rec_type=="augmented_recognition":
+        for name in images:
+            responses=[]
+            img=cv2.imread(f"{ds_dir}/{name}",cv2.COLOR_BGR2GRAY)
+            img=cv2.medianBlur(img,3)
+            for angle in range(-20, 21):
+                # Получение размеров изображения
+                (h, w) = img.shape[:2]
+
+                # Определение центра изображения
+                center = (w // 2, h // 2)
+
+                # Создание матрицы поворота
+                M = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+                # Вычисление новой ширины и высоты, чтобы избежать обрезки
+                cos = np.abs(M[0, 0])
+                sin = np.abs(M[0, 1])
+
+                # Новые размеры
+                new_w = int((h * sin) + (w * cos))
+                new_h = int((h * cos) + (w * sin))
+
+                # Корректировка матрицы поворота для смещения центра
+                M[0, 2] += new_w / 2 - center[0]
+                M[1, 2] += new_h / 2 - center[1]
+
+                # Поворот изображения с новыми размерами
+                rotated_image = cv2.warpAffine(img, M, (new_w, new_h))
+                text = tes.image_to_string(rotated_image, lang="rus+eng")
+                responses.append(text)
+            counted=Counter(responses)
+            if '' in counted:
+                del counted['']
+            #print(counted)
+            most_common,_=counted.most_common()[0]
+            #print(most_common)
+            res[name[:-4]]=str(most_common).replace("\n","")
 
     if val_type=="accuracy":
         count=0
@@ -81,6 +118,7 @@ def test_recognition(rec_type,val_type,ds_dir):
     for i in labels.keys():
         reswriter.write(f'{res[i]} : {labels[i]}\n')
 
-test_recognition("straight_recognition","wer","ds2")
 #augmentation_ds("ds1")
 #create_agds_txt()
+# test_recognition("straight_recognition","wer","ds2")
+test_recognition("augmented_recognition","wer","ds1")
